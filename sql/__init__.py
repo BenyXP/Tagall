259acc057aac52e16c5cc29f03d1bf2076da5888
BenyXP <103335260+BenyXP@users.noreply.github.com>
2023-01-19 00:27:43 +0700

    Create __init__.py

---
+++ sql/__init__.py
@@ -0,0 +1,20 @@
+from os import environ
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+DB_URI = environ.get("DATABASE_URL", "")
+if DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
