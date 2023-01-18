e9c28cd4ec6707b1db3ea106c99d7c32beea36ea
BenyXP <103335260+BenyXP@users.noreply.github.com>
2023-01-19 00:31:44 +0700

    Update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -1,13 +1,10 @@
-from os import environ
-
+import os
+import threading
 from sqlalchemy import create_engine
+from sqlalchemy import Column, TEXT, Numeric
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-DB_URI = environ.get("DATABASE_URL", "")
-if DB_URI.startswith("postgres://"):
-    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
-
+from config import DB_URI
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
@@ -18,3 +15,40 @@ def start() -> scoped_session:
 
 BASE = declarative_base()
 SESSION = start()
+
+INSERTION_LOCK = threading.RLock()
+
+class Broadcast(BASE):
+    __tablename__ = "broadcast"
+    id = Column(Numeric, primary_key=True)
+    user_name = Column(TEXT)
+
+    def __init__(self, id, user_name):
+        self.id = id
+        self.user_name = user_name
+
+Broadcast.__table__.create(checkfirst=True)
+
+
+#  Add user details -
+async def add_user(id, user_name):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Broadcast).get(id)
+        if not msg:
+            usr = Broadcast(id, user_name)
+            SESSION.add(usr)
+            SESSION.commit()
+        else:
+            pass
+          
+async def full_userbase():
+    users = SESSION.query(Broadcast).all()
+    SESSION.close()
+    return users
+
+async def query_msg():
+    try:
+        query = SESSION.query(Broadcast.id).order_by(Broadcast.id)
+        return query
+    finally:
+        SESSION.close()
